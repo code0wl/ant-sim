6b9331f82ceef4f67b5a7bcdf6c6c876d160660d
Oscar Lodriguez <o.lodriguez@gmail.com>
2018-12-28 20:06:47 +0100

    refactor game render

--- src/game/game.ts
+++ src/game/game.ts
@@ -1,15 +1,39 @@
+declare const cc: any;
+
 export class Game {
     public canvas: HTMLCanvasElement;
 
-    constructor(width, height) {
+    constructor(width, height, id) {
         this.generateWorld(width, height);
+
+        cc.game.onStart = function() {
+            //load resources
+            cc.LoaderScene.preload(
+                ["HelloWorld.png"],
+                () => {
+                    var MyScene = cc.Scene.extend({
+                        onEnter: function() {
+                            this._super();
+                            var size = cc.director.getWinSize();
+                            var sprite = cc.Sprite.create("HelloWorld.png");
+                            sprite.setPosition(size.width / 2, size.height / 2);
+                            sprite.setScale(0.8);
+                            this.addChild(sprite, 0);
+                        },
+                    });
+                    cc.director.runScene(new MyScene());
+                },
+                this
+            );
+        };
+        cc.game.run(id);
     }
 
-    private generateWorld(width, height) {
+    private generateWorld(width, height, id) {
         this.canvas = document.createElement("canvas");
         this.canvas.width = width;
         this.canvas.height = height;
-        this.canvas.id = "gameCanvas";
+        this.canvas.id = id;
         document.body.appendChild(this.canvas);
     }
 
--- src/index.ts
+++ src/index.ts
@@ -2,36 +2,4 @@ declare const cc: any;
 
 import { Game } from "./game/game";
 
-const game = new Game(window.innerWidth, window.innerHeight);
-
-window.onload = function() {
-    cc.game.onStart = function() {
-        //load resources
-        cc.LoaderScene.preload(
-            ["HelloWorld.png"],
-            () => {
-                var MyScene = cc.Scene.extend({
-                    onEnter: function() {
-                        this._super();
-                        var size = cc.director.getWinSize();
-                        var sprite = cc.Sprite.create("HelloWorld.png");
-                        sprite.setPosition(size.width / 2, size.height / 2);
-                        sprite.setScale(0.8);
-                        this.addChild(sprite, 0);
-
-                        var label = cc.LabelTTF.create(
-                            "Hello World",
-                            "Arial",
-                            40
-                        );
-                        label.setPosition(size.width / 2, size.height / 2);
-                        this.addChild(label, 1);
-                    },
-                });
-                cc.director.runScene(new MyScene());
-            },
-            this
-        );
-    };
-    cc.game.run("gameCanvas");
-};
+const game = new Game(window.innerWidth, window.innerHeight, "gameCanvas");
