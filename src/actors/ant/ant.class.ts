59296396137374c403be5866996a56784cc542f4
Oscar Lodriguez <o.lodriguez@gmail.com>
2018-12-29 09:46:17 +0100

    creatures rocks

--- src/actors/ant/ant.class.ts
+++ src/actors/ant/ant.class.ts
@@ -1,14 +1,21 @@
 import { Animal } from "common/animal.class";
 import { Coordinates } from "common/model";
 
+declare const cc: any;
+
 export class Ant extends Animal {
+    readonly graphic: any;
     readonly currentLocation: Coordinates;
+
     public isAlive = true;
     public isMoving = false;
 
     constructor() {
         super();
-        console.log("ant");
+        this.graphic = new cc.SpriteFrame(
+            "assets/HelloWorld.png",
+            cc.rect(0, 0, 90, 128)
+        );
     }
 
     public move() {}
