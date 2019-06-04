862488d08d36ad0bde9bba4023e7e7e6e334cf6d
Oscar Lodriguez <o.lodriguez@gmail.com>
2019-06-04 14:20:48 +0200

    grid

--- src/engine/modules/draw/grid.ts
+++ src/engine/modules/draw/grid.ts
@@ -2,13 +2,12 @@ import { IMap } from "game/model";
 import { IAnimal, Point } from "common/model";
 
 export class Grid {
-    gridSize: number = 40;
+    gridSize: number = 20;
     // add interface
     rows: any = [];
     cols: any = [];
     cells: any = [];
 
-    public cells: number[];
     constructor(public canvas: HTMLCanvasElement, dimensions: Point) {
         this.drawGrid(dimensions);
     }
@@ -24,26 +23,26 @@ export class Grid {
 
     private drawGrid({ x, y }: Point) {
         const ctx = this.canvas.getContext("2d");
+        ctx.strokeStyle = "#006400";
 
         // cols
-        for (let i = 0, j = 0; i < x; i += this.gridSize) {
+        for (let i = 0, j = 0; i <= x; i += this.gridSize) {
             this.cols[j] = { cell: j, coordinates: i };
             ctx.moveTo(i, 0);
-            ctx.lineTo(i, x);
+            ctx.lineTo(i, y);
             j += 1;
         }
 
         // rows: any
-        for (let i = 0, j = 0; i < y; i += this.gridSize) {
+        for (let i = 0, j = 0; i <= y; i += this.gridSize) {
             this.rows[j] = { cell: j, coordinates: i };
             ctx.moveTo(0, i);
             ctx.lineTo(x, i);
             j += 1;
         }
-        ctx.strokeStyle = "#006400";
         ctx.stroke();
 
-        console.log(this.rows)
-        console.log(this.cols)
+        console.log("rows", this.rows);
+        console.log("cols", this.cols);
     }
 }
