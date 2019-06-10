92042fd3681b12bfa055a482f99114ced630cd66
Oscar Lodriguez <o.lodriguez@gmail.com>
2019-06-10 14:19:47 +0200

    fod detect fix

--- src/game/actors/food/food.ts
+++ src/game/actors/food/food.ts
@@ -3,20 +3,21 @@ import { Point } from "engine/modules/draw/point";
 import { Colors, actorType } from "common/model";
 
 export class Food extends Actor {
-    public amount = 30;
+    public width = 10;
+    public height = 10;
 
     constructor(public coordinates: Point) {
         super(actorType.food);
     }
 
     draw(ctx: CanvasRenderingContext2D) {
-        if (!this.amount) return;
+        if (!this.width) return;
         ctx.fillStyle = Colors.food;
         ctx.fillRect(
             this.coordinates.x,
             this.coordinates.y,
-            this.amount,
-            this.amount
+            this.width,
+            this.height
         );
     }
 }
