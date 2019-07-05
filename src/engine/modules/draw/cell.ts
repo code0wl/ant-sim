939668e2cc750f49585e6678d66002a6e1cd3bf5
Oscar Lodriguez <o.lodriguez@gmail.com>
2019-07-05 23:05:51 +0200

    color

--- src/engine/modules/draw/cell.ts
+++ src/engine/modules/draw/cell.ts
@@ -1,14 +1,15 @@
 // TODO refactor
 export class Cell {
-    public hasSpider: boolean = false;
-    public hasAnt: boolean = false;
-    public hasFood: boolean = false;
-    public hasBigSpider: boolean = false;
-    public hasAlertScent: boolean = false;
-    public hasRedAnt: boolean = false;
-    public hasNest: boolean = false;
-    public hasSmallSpider: boolean = false;
-    public hasBlackAnt: boolean = false;
+    public hasSpider = false;
+    public hasAnt = false;
+    public hasFood = false;
+    public hasBigSpider = false;
+    public hasAlertScent = false;
+    public hasRedAnt = false;
+    public hasNest = false;
+    public hasSmallSpider = false;
+    public hasBlackAnt = false;
+    public hasFoodPhermone = false;
 
     constructor(public x: number, public y: number) {}
 }
